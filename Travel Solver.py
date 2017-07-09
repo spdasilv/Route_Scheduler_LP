78d5fc318784d77edd9b3d957593a24f6a91406e
spdasilv <stefannopolo@yahoo.com.br>
2017-07-09 04:02:36 -0400

    Like a dog with 3 legs

--- Travel Solver.py
+++ Travel Solver.py
@@ -37,7 +37,7 @@ model.j = SetOf(model.i)
 #         /    0     6
 #              1     8  /;
 
-time_values = [0, 1, 1, 1, 1]
+time_values = [0, 2, 2, 2, 2]
 Ti = {}
 for i in range(0, len(time_values)):
     Ti[i] = time_values[i]
@@ -115,21 +115,28 @@ model.Sijdt = Var(model.i, model.j, model.d, model.t, within=Binary, doc='Follow
 
 
 ## Define constraints ##
-def Availability(model, d, t):
-    return sum(model.Sijdt[i, j, d, t] for i in model.i for j in model.j) <= model.Adt[d, t]
-model.Availability = Constraint(model.d, model.t, rule=Availability, doc='Availability')
+def Availability(model, i, j, d, t):
+    tmax = 95 if t + model.Cij[i, j] + model.Ti[j] >= 95 else t + model.Cij[i, j] + model.Ti[j]
+    return model.Sijdt[i, j, d, t] <= model.Adt[d, tmax]
+model.Availability = Constraint(model.i, model.j, model.d, model.t, rule=Availability, doc='Availability')
 
-
-def IsOpen(model, i, d, t):
-    return sum(model.Yijdt[i, j, d, t] for j in model.j) <= model.Oidt[i, d, t]
-model.IsOpen = Constraint(model.i, model.d, model.t, rule=IsOpen, doc='Continue if Open')
-
-
+def IsOpen(model,i, j, d, t):
+    tmax = 95 if t + model.Cij[i, j] + model.Ti[j] >= 95 else t + model.Cij[i, j] + model.Ti[j]
+    return model.Sijdt[i, j, d, t] <= model.Oidt[j, d, tmax]
+model.IsOpen = Constraint(model.i, model.j, model.d, model.t, rule=IsOpen, doc='Is Open')
+#
+#
 def startOnce(model, i):
     if i == 0:
         i = 1
     return sum(model.Sijdt[i, j, d, t] for j in model.j for d in model.d for t in model.t) <= 1
 model.startOnce = Constraint(model.i, rule=startOnce, doc='Start Activity Once')
+
+def endtOnce(model, j):
+    if j == 0:
+        j = 1
+    return sum(model.Sijdt[i, j, d, t] for i in model.i for d in model.d for t in model.t) <= 1
+model.endtOnce = Constraint(model.j, rule=endtOnce, doc='End Activity Once')
 
 
 def startAtHotel(model, d):
@@ -140,32 +147,38 @@ model.startAtHotel = Constraint(model.d, rule=startAtHotel, doc='Start at Hotel'
 def endAtHotel(model, d):
     return sum(model.Sijdt[i, 0, d, t] for i in model.i for t in model.t) == 1
 model.endAtHotel = Constraint(model.d, rule=endAtHotel, doc='End at Hotel')
-
-
+#
+#
 def circularRule(model, d, t):
     return sum(model.Sijdt[i, i, d, t] for i in model.i) == 0
 model.circularRule = Constraint(model.d, model.t, rule=circularRule, doc='No Circles')
 
 
 def CompAct(model, i, j, d, t):
-    tmax = 96 if t + model.Cij[i, j] + model.Ti[i] + 1 >= 96 else t + model.Cij[i, j] + model.Ti[i] + 1
-    return (model.Cij[i, j] + model.Ti[i])*model.Sijdt[i, j, d, t] <= sum(model.Yijdt[i, j, d, g] for g in range(t, tmax))
+    tmax = 96 if t + model.Cij[i, j] + model.Ti[j] + 1 >= 96 else t + model.Cij[i, j] + model.Ti[j] + 1
+    return sum(model.Sijdt[h, f, d, g] for h in model.i for f in model.j for g in range(t + 1, tmax)) + model.Sijdt[i, j, d, t] <= 1
 model.CompAct = Constraint(model.i, model.j, model.d, model.t, rule=CompAct, doc='Complete Activity')
 
 
 def timeAvailable(model, d):
-    return sum((model.Cij[i, j] + model.Ti[i])*model.Sijdt[i, j, d, t] for i in model.i for j in model.j for t in model.t) <= model.Rd[d]
-#     return sum(model.Cij[i, j]*model.Sijdt[i, j, d, t] for i in model.i for j in model.j for t in model.t) + sum(model.Ti[i]*sum(model.Sijdt[i, j, d, t] for j in model.j for t in model.t) for i in model.i) + sum(model.Cij[0, j]*sum(model.Sijdt[0, j, d, t] for t in model.t) for j in model.j) + sum(model.Cij[i, 0]*sum(model.Sijdt[i, 0, d, t] for t in model.t) for i in model.i) <= model.Rd[d]
+    return sum((model.Cij[i, j] + model.Ti[j])*model.Sijdt[i, j, d, t] for i in model.i for j in model.j for t in model.t) <= model.Rd[d]
 model.timeAvailable = Constraint(model.d, rule=timeAvailable, doc='Time Available')
-#
-#
+
+
 def limitActivities(model, d, t):
-    return sum(model.Yijdt[i, j, d, t] for i in model.i for j in model.j) <= 1
+    return sum(model.Sijdt[i, j, d, t] for i in model.i for j in model.j) <= 1
 model.limitActivities = Constraint(model.d, model.t, rule=limitActivities, doc='Schedule Activities')
+
+
+def Continuity(model, i ,j, d, t):
+    if j == 0:
+        j = 1
+    return sum(model.Sijdt[j, h, d, g] for g in range(t + 1, 96) for h in model.i) >= model.Sijdt[i, j, d, t]
+model.Continuity = Constraint(model.i, model.j, model.d, model.t, rule=Continuity, doc='Continuity')
 
 ## Define Objective and solve ##
 def objectiveRule(model):
-    return sum(model.Wi[i]*sum(model.Sijdt[i, j, d, t] for j in model.j for d in model.d for t in model.t) for i in model.i)
+    return sum(model.Wi[j]*sum(model.Sijdt[i, j, d, t] for i in model.i for d in model.d for t in model.t) for j in model.j)
 
 
 model.objectiveRule = Objective(rule=objectiveRule, sense=maximize, doc='Define Objective Function')
